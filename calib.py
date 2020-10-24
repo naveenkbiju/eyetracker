4fe3b1f9ab0c58d485575e08822dab39e17e6294
lakshmi_11 <parvathylakshmi444@gmail.com>
2020-10-24 13:56:24 +0530

    UI modified

--- calib.py
+++ calib.py
@@ -5,21 +5,22 @@ import sys
 
 
 class Window(QWidget):
-    def _init_(self):
-        super()._init_()
+    def __init__(self):
+        super().__init__()
         self.setWindowTitle("Python ")
         self.showFullScreen()
+        self.data = {}
+        self.check = []
+        self.c = 0;
+        self.re = []
         palette = QPalette()
-        palette.setColor(QPalette.Background, Qt.black)
+        palette.setColor(QPalette.Background, Qt.white)
         self.setPalette(palette)
         self.ht = self.height()
         self.wt = self.width()
         print(str(self.ht)+ str(self.wt))
         self.UiComponents()
         self.show()
-        self.data = {}
-        self.check = []
-        self.c = 0;
     def UiComponents(self):
         button = []
         k=0
@@ -31,22 +32,27 @@ class Window(QWidget):
                 y = i*half_ht
                 button.append(QPushButton(str(k+1), self))
                 button[k].setGeometry(x,y , 30, 30)
-                button[k].setStyleSheet("border-radius :15;border: 1px solid white")
-                button[k].clicked.connect(self.calib(k+1,x,y))
+                button[k].setStyleSheet(open('buttonStyle.css').read())
+                button[k].clicked.connect(self.calib(button ,k+1,x,y))
                 button[k].show()
+                self.re.append(0)
                 k=k+1
 
-    def calib(self, k,x,y):
+    def calib(self,button ,k,x,y):
         def add_data():
-            self.data[k] = (x,y)
-            print(k)
-            if k not in self.check:
-                self.c= self.c+1
-                self.check.append(k)
-            if(self.c ==  9 ):
-                print("calibration completed")
-                print(self.data)
-                sys.exit()
+            if(self.re[k-1] != 5):
+                self.re[k-1] = self.re[k-1] + 1
+            else:
+                button[k-1].setStyleSheet("background-color: red")
+                self.data[k] = (x, y)
+                print(k)
+                if k not in self.check:
+                    self.c = self.c + 1
+                    self.check.append(k)
+                if (self.c == 9):
+                    print("calibration completed")
+                    print(self.data)
+                    sys.exit()
         return add_data
 
 App = QApplication(sys.argv)
