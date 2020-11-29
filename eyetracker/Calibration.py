66fb6b3fb4f4da81a6d02e12b7d65a1ba7e08478
lakshmi_11 <parvathylakshmi444@gmail.com>
2020-11-29 21:14:06 +0530

    Recalibrate feature added and UI updated

--- eyetracker/Calibration.py
+++ eyetracker/Calibration.py
@@ -30,13 +30,15 @@ class CalibrationModel(QWidget):
         self.start_webcam()
         self.app_shortcut()
         self.show()
+        self.infoDialog("click each points 5 times" , "EyeTracker" , "press Q to quit \n press R to recalibrate")
     def initialisation(self):
         self.x_train = []
         self.y_train = []
     def app_shortcut(self):
         self.shortcut_close = QShortcut(QKeySequence('Q'), self)
-        self.shortcut_close.activated.connect(self.closeApp)
-
+        self.shortcut_close.activated.connect(self.closeAppDialog)
+        self.shortcut_recalibrate = QShortcut(QKeySequence('R'), self)
+        self.shortcut_recalibrate.activated.connect(self.recalibrateDialog)
     def start_webcam(self):
         self.video = Video()
         self.video.start_webcam()
@@ -54,60 +56,33 @@ class CalibrationModel(QWidget):
         self.buttonRepeat = []
         self.buttonComplete = 0
         k = 0
-        rad = 30
-        self.half_ht = int((self.ht - rad) / 2)
-        self.half_wt = int((self.wt - rad) / 2)
+        self.rad = 20
+        self.half_ht = int((self.ht - self.rad) / 2)
+        self.half_wt = int((self.wt - self.rad) / 2)
         for i in range(3):
             for j in range(3):
                 x = j * self.half_wt
                 y = i * self.half_ht
-                self.button.append(QPushButton(str(k + 1), self))
-                self.button[k].setGeometry(x, y, rad, rad)
+                self.button.append(QPushButton("O", self))
+                self.button[k].setGeometry(x, y, self.rad, self.rad)
                 self.button[k].setStyleSheet(open(self.getPath("stylesheet/buttonStyle.css")).read())
                 self.button[k].clicked.connect(self.ButtonPress(k, x, y))
                 self.button[k].show()
                 self.buttonRepeat.append(0)
                 k = k + 1
-    def hideButtons(self,  notkey  = None):
+        self.buttonCenter = QPushButton("O", self)
+        self.buttonCenter.setGeometry(self.half_wt, self.half_ht, self.rad , self.rad)
+        self.buttonCenter.setStyleSheet("background-color: red ; border-radius: 10px;")        
+    def hideButtons(self):
         for i  in self.button:
-            if(i != self.button[notkey]):
-                i.hide()
-    def showRetryDialog(self):
-        logging.warn("Eyes not detected")
-        msg = QMessageBox()
-        msg.setIcon(QMessageBox.Information)
-        msg.setText("Eyes Not Detected")
-        msg.setInformativeText("Place your Eyes in camera")
-        msg.setWindowTitle("warning")
-        msg.setStandardButtons(QMessageBox.Retry)
-        msg.exec_()
-    def infoDialog(self,text , title):
-        msg = QMessageBox()
-        msg.setIcon(QMessageBox.Information)
-        msg.setText(text)
-        msg.setWindowTitle(title)
-        msg.setInformativeText(title)
-        msg.setStandardButtons(QMessageBox.Ok)
-        msg.buttonClicked.connect(self.do_nothing)
-        msg.exec_()
-    def accuracyTestMessageBox(self):
-        msg = QMessageBox()
-        msg.setIcon(QMessageBox.Information)
-        msg.setText("calibration completed")
-        msg.setInformativeText("do you want to test accuracy ?")
-        msg.setWindowTitle("Accuracy test")
-        msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
-        msg.buttonClicked.connect(self.testAccuracy)
-        msg.exec_()
-    def do_nothing(self):
-        pass    
+            i.hide()
     def ButtonPress(self, k, x, y):
         def calib():
             logging.info("button pressed")
             if self.video.is_eye_detected():
                 self.add_data(x, y)
                 if self.isFiveTimes(k):
-                    self.button[k].setStyleSheet("background-color: red ")
+                    self.button[k].setStyleSheet("background-color: red ; ; border-radius: 10px")
                 if self.isCalibComplete():
                     self.save()
                     print(self.accuracyTest)
@@ -115,7 +90,6 @@ class CalibrationModel(QWidget):
                         self.accuracyTestMessageBox()
                     else :    
                         self.infoDialog("calibration completed "  , "EyeTracking")
-                        self.closeApp()
             else:
                 self.showRetryDialog()
 
@@ -148,8 +122,8 @@ class CalibrationModel(QWidget):
             logging.error("calibration file not found")
     def testAccuracy(self):
         self.infoDialog("look at the center point for some time" , "Accuracy Test")
-        centerButtonKey  = 5
-        self.hideButtons(notkey = centerButtonKey -1)
+        self.hideButtons()
+        self.buttonCenter.show()
         self.predict = GazePredict()
         self.predict.train()
         self.data = []
@@ -173,12 +147,69 @@ class CalibrationModel(QWidget):
         centerPoint = self.half_wt , self.half_ht
         test_value = "Accuracy : "  + str(accuracy.checkAccuracy(centerPoint,self.ht))
         self.infoDialog(test_value , "Accuracy Test")
-        self.closeApp()
+        self.recalibrateDialog()
     def paintEvent(self , event):
         qp = QPainter(self)
-        qp.setPen(QPen(Qt. blue , 8 , Qt.SolidLine))
+        qp.setBrush(QBrush(Qt.red, Qt.SolidPattern))
         for i in self.data :
             x,y = i
-            qp.drawEllipse(x,y, 30 , 30)   
+            qp.drawEllipse(x,y, 15 , 15)
+    def recalibrate(self):
+        self.data = []
+        self.update()
+        self.buttonCenter.hide()
+        
+        self.buttonComplete = 0
+        for k in range(9):
+            self.buttonRepeat[k] = 0
+            self.button[k].setStyleSheet(open(self.getPath("stylesheet/buttonStyle.css")).read())
+            self.button[k].show()           
     def closeApp(self):
         self.App.exit()
+    def showRetryDialog(self):
+        logging.warn("Eyes not detected")
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText("Eyes Not Detected")
+        msg.setInformativeText("Place your Eyes in camera")
+        msg.setWindowTitle("warning")
+        msg.setStandardButtons(QMessageBox.Retry)
+        msg.exec_()
+    def infoDialog(self,text , title,info = ""):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText(text)
+        msg.setWindowTitle(title)
+        msg.setInformativeText(info)
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.exec_()
+    def closeAppDialog(self):
+        logging.warning("Recalibrate")
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText("Do you want to close app ?")
+        msg.setWindowTitle("EyeTracker")
+        msg.setStandardButtons(QMessageBox.Close | QMessageBox.Cancel )
+        if(msg.exec_() == QMessageBox.Close):
+            self.closeApp()               
+    def recalibrateDialog(self):
+        logging.warning("Recalibrate")
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText("Do you want to recalibrate ?")
+        msg.setInformativeText("Do you want to recalibrate ?")
+        msg.setWindowTitle("EyeTracker")
+        msg.setStandardButtons(QMessageBox.Abort | QMessageBox.Yes )
+        if(msg.exec_() == QMessageBox.Yes):
+            self.recalibrate()
+        else:
+            self.closeApp()        
+    def accuracyTestMessageBox(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText("calibration completed")
+        msg.setInformativeText("do you want to test accuracy ?")
+        msg.setWindowTitle("Accuracy test")
+        msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        msg.buttonClicked.connect(self.testAccuracy)
+        msg.exec_() 
